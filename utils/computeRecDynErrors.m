9464703839cd7685695b57f90fb0b4bb5e3323cb
Joar Axås <jgoeransson@ethz.ch>
2021-03-08 13:54:10 +0100

    Update error computation for reduced dynamics

--- utils/computeRecDynErrors.m
+++ utils/computeRecDynErrors.m
@@ -5,6 +5,6 @@ reducedTrajDist = zeros(nTraj,1);
 fullTrajDist = zeros(nTraj,1); 
 for iTraj = 1:nTraj
     nPoints = length(yData{iTraj,1});
-    reducedTrajDist(iTraj) = norm(yRec{iTraj,2} - yData{iTraj,2}) / nPoints;
-    fullTrajDist(iTraj) = norm(xRec{iTraj,2} - xData{iTraj,2}) / nPoints;
+    reducedTrajDist(iTraj) = sum(vecnorm(yRec{iTraj,2} - yData{iTraj,2}, 2)) / nPoints / max(vecnorm(yData{iTraj,2},2));
+    fullTrajDist(iTraj) = sum(vecnorm(xRec{iTraj,2} - xData{iTraj,2}, 2)) / nPoints / max(vecnorm(xData{iTraj,2},2));
 end
