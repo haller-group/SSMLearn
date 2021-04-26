24ed2d71e5cf8d2d65a5080d2dae64a5e2557023
Joar Axås <jgoeransson@ethz.ch>
2021-04-26 18:05:44 +0200

    Add manifold plotting for 1D

--- examples/stuartLandau.m
+++ examples/stuartLandau.m
@@ -1,13 +1,14 @@
 clearvars
 close all
 
-IC = [1e-3, 0.25];
+IC = [1e-3, -1e-3];
 nTraj = size(IC, 2);
-indTrain = 2;
+indTrain = 1;
 indTest = 1;
-SSMDim = 2;
+SSMDim = 1;
 
-F = @(t,R) R-R.^3;
+mu = 1;
+F = @(t,R) mu*R-R.^3;
 
 observable = @(x) x;
 tEnd = 20;
@@ -16,41 +17,61 @@ nSamp = tEnd/dt+1;
 
 xData = integrateTrajectories(F, observable, tEnd, nSamp, nTraj, IC);
 %%
-overEmbed = -3;
-SSMOrder = 5;
+overEmbed = 0;
+SSMOrder = 3;
 
 yData = coordinates_embedding(xData, SSMDim, 'OverEmbedding', overEmbed, 'ShiftSteps', 1);
+% yData = xData;
 
-% [V, SSMFunction, mfdInfo] = IMparametrization(yData(indTrain,:), SSMDim, SSMOrder);
-V = eye(2);
-SSMFunction = @(q)V*q;
+[V, SSMFunction, mfdInfo] = IMparametrization(yData(indTrain,:), SSMDim, SSMOrder);
+% V = eye(1);
+% SSMFunction = @(q)V*q;
 %%
-% etaData = getProjectedTrajs(yData, V);
-etaData = yData;
-plotReducedCoords(etaData);
+etaData = getProjectedTrajs(yData, V);
+% etaData = yData;
+% plotReducedCoords(etaData);
 
 RRMS = getRMS(yData(indTest,:), SSMFunction, V)
 
-% yLifted = liftReducedTrajs(etaData, SSMFunction);
-% plotReconstructedTrajectory(yData(indTest(1),:), yLifted(indTest(1),:), 1)
+yLifted = liftReducedTrajs(etaData, SSMFunction);
+plotReconstructedTrajectory(yData(indTest(1),:), yLifted(indTest(1),:), 1)
 
-% plotSSMWithTrajectories(yData(indTrain,:), SSMFunction, [1,2,3], V, 15000, 'SSMDimension', SSMDim)
-% axis equal
-% view(50, 30)
+plotSSMWithTrajectories(yData, SSMFunction, [1,2,3], V, 10, 'SSMDimension', SSMDim)
+view(50, 30)
 %% Reduced dynamics
 ROMOrder = 3;
-[R,~,~,~,MapsInfo] = IMdynamics_map(etaData(indTrain,:), 'R_PolyOrd', ROMOrder);
+[R,~,~,~,MapsInfo] = IMdynamics_flow(etaData(indTrain,:), 'R_PolyOrd', ROMOrder);
+MapsInfo.R.coeff
 
-[etaRec, yRec] = iterateMaps(R, etaData, @(eta) SSMFunction(eta));
+%%
+figure
+x = linspace(-1,1.5)';
+plot(x, x-x.^3, 'DisplayName', 'True', 'LineWidth', 2)
+hold on
+plot(x,sum(MapsInfo.R.coeff.*x.^(1:ROMOrder),2), ':', 'DisplayName', ['ROM order ' num2str(ROMOrder)], 'LineWidth', 2)
+xlabel('$R$', 'Interpreter', 'latex')
+ylabel('$\dot{R}$', 'Interpreter', 'latex')
+legend()
+set(gca, 'fontname', 'times')
+set(gca, 'fontsize', 18)
+
+figure
+plot(xData{1,1}, xData{1,2}, 'LineWidth', 2)
+xlabel('$t$ [s]', 'Interpreter', 'latex')
+ylabel('$R$', 'Interpreter', 'latex')
+set(gca, 'fontname', 'times')
+set(gca, 'fontsize', 18)
+
+%%
+[etaRec, yRec] = integrateFlows(R, etaData, @(eta) SSMFunction(eta));
 
 [reducedTrajDist, fullTrajDist] = computeRecDynErrors(etaRec, yRec, etaData, yData);
 
 RMSE = mean(reducedTrajDist(indTest))
 RRMSE = mean(fullTrajDist(indTest))
 
-plotReducedCoords(etaData(indTest(1),:), etaRec(indTest(1),:))
-plotReconstructedTrajectory(yData(indTest(1),:), yRec(indTest(1),:), 2, 'm')
+plotReconstructedTrajectory(yData(indTest(1),:), yRec(indTest(1),:), 1, 'm')
 
-reconstructedEigenvalues = computeEigenvaluesMap(MapsInfo, dt)
-% DSEigenvalues = lambda(1:SSMDim)
+reconstructedEigenvalues = computeEigenvaluesFlow(MapsInfo)
+DSEigenvalues = mu
 
--- utils/plotSSMWithTrajectories.m
+++ utils/plotSSMWithTrajectories.m
@@ -20,19 +20,27 @@ hold on
 if opts.SSMDimension == 2
     etaData = transpose(V) * cat(2,yData{:,2});
     plot_2dSSM_surf(plotInds, etaData, SSMFunction, radiusIncr, 50, 0);
+elseif opts.SSMDimension == 1
+    etaData = transpose(V) * cat(2,yData{:,2});
+    etaMin = - max(abs(etaData)) * (100+radiusIncr)/100;
+    etaMax = + max(abs(etaData)) * (100+radiusIncr)/100;
+    parametrization = linspace(etaMin, etaMax);
+    dVals = SSMFunction(parametrization);
+    mfdplot = plot3(dVals(plotInds(1),:), dVals(plotInds(2),:), dVals(plotInds(3),:), 'b', 'LineWidth', 7);
+    mfdplot.Color(4) = 0.1;
 else
-    disp("SSM plotting only available for 2D manifolds")
+    disp("SSM plotting only available for 1D and 2D manifolds")
 end
 
 for iTraj = 1:size(yData,1)
     if length(plotInds) == 3
-        plot3(yData{iTraj,2}(plotInds(1),:), yData{iTraj,2}(plotInds(2),:), yData{iTraj,2}(plotInds(3),:))
+        plot3(yData{iTraj,2}(plotInds(1),:), yData{iTraj,2}(plotInds(2),:), yData{iTraj,2}(plotInds(3),:), 'LineWidth', 2)
         xlabel(['$y_{' num2str(plotInds(1)) '}$'], 'Interpreter', 'latex')
         ylabel(['$y_{' num2str(plotInds(2)) '}$'], 'Interpreter', 'latex')
         zlabel(['$y_{' num2str(plotInds(3)) '}$'], 'Interpreter', 'latex')
     elseif length(plotInds) == 1
         etaData = getProjectedTrajs(yData, V);
-        plot3(etaData{iTraj,2}(1,:), etaData{iTraj,2}(2,:), yData{iTraj,2}(plotInds,:))
+        plot3(etaData{iTraj,2}(1,:), etaData{iTraj,2}(2,:), yData{iTraj,2}(plotInds,:), 'LineWidth', 2)
         xlabel(['$\eta_1$'], 'Interpreter', 'latex')
         ylabel(['$\eta_2$'], 'Interpreter', 'latex')
         zlabel(['$y_{' num2str(plotInds) '}$'], 'Interpreter', 'latex')
