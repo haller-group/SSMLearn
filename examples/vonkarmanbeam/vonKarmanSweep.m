3984046341989e19ea10e66aae54fced90083bad
Yogapig <joar.axas@gmail.com>
2021-04-29 14:01:14 +0100

    FRC Sweep add FRC_full

--- examples/vonkarmanbeam/vonKarmanSweep.m
+++ examples/vonkarmanbeam/vonKarmanSweep.m
@@ -12,13 +12,14 @@ b       = 50e-3;  % width
 
 [M,C,K,fnl,fext,outdof] = von_karman_model(nElements, E, rho, nu, kappa, l, h, b);
 n = size(M,1);    % mechanical dofs (axial def, transverse def, angle)
-omega = linspace(95,110,10);
+w_span = [95,125];
+omega = linspace(min(w_span),max(w_span),1);
 
-w0 = -K\fext; % linear initial guess
+w0 = -K\(7.0*fext); % linear initial guess
 IC = [w0; zeros(n,1)];
 
 for ii = 1:length(omega)
-[F, lambda] = functionFromTensors(M, C, K, fnl, 3.5*fext, omega(ii));
+[F, lambda] = functionFromTensors(M, C, K, fnl, 7.0*fext, omega(ii));
 observable = @(x) x;
 tEnd = 100*2*pi/omega(ii);
 nSamp = fix(50 * tEnd * abs(imag(lambda(1))) / (2*pi));
@@ -30,4 +31,11 @@ toc
 ampli(ii) = max(abs(xData{1,2}(n-1,end-100:end)))
 IC = xData{1,2}(:,end);
 end
+
+f_full = [7.0];
+FRC_full = getFRC_full(M, C, K, fnl, fext, f_full, n-1, w_span, 7); close all
+
+%%
 plot(omega,ampli,'o')
+hold on
+plot(FRC_full.F1.Freq, FRC_full.F1.Amp)
