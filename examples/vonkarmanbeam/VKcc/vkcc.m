7e2b9762e239e43175136100303aa77028df3676
Yogapig <joar.axas@gmail.com>
2021-05-14 07:39:45 +0100

    static calibration

--- examples/vonkarmanbeam/VKcc/vkcc.m
+++ examples/vonkarmanbeam/VKcc/vkcc.m
@@ -10,7 +10,7 @@ close all
 clc
 %% Example setup
 
-nElements = 12;
+nElements = 6;
 [M, C, K, fnl, f_vec, outdof, PlotFieldonDefMesh] = build_model(nElements);
 n = size(M,1);    % mechanical dofs (axial def, transverse def, angle)
 [F, lambda] = functionFromTensors(M, C, K, fnl);
@@ -53,7 +53,7 @@ end
 %% Delay embedding
 
 SSMDim = 2;
-overEmbed = 0;
+overEmbed = 30;
 yData = coordinates_embedding(xData, SSMDim, 'OverEmbedding', overEmbed);
 %% 
 
@@ -72,19 +72,20 @@ etaDataTrunc = getProjectedTrajs(yDataTrunc, V);
 ROMOrder = 7;
 [~,Tinv,N,T,NormalFormInfo] = IMdynamics_flow(etaDataTrunc(indTrain,:),'R_PolyOrd',ROMOrder,'style', 'normalform');
 
-zData = transformComplex(Tinv, etaDataTrunc);
-[zRec, yRecNormal] = integrateFlows(N, zData, @(q) SSMFunction(T(q)));
-etaRecNormal = transformComplex(T, zRec);
+zData = transformComplex(Tinv, etaData);
+zDataTrunc = transformComplex(Tinv, etaDataTrunc);
+[zRec, yRec] = integrateFlows(N, zDataTrunc, @(q) SSMFunction(T(q)));
+etaRec = transformComplex(T, zRec);
 
-[reducedTrajDist, fullTrajDist] = computeRecDynErrors(etaRecNormal, yRecNormal, etaDataTrunc, yDataTrunc);
+[reducedTrajDist, fullTrajDist] = computeRecDynErrors(etaRec, yRec, etaDataTrunc, yDataTrunc);
 RRMSE_normal = mean(fullTrajDist(indTest))
 %% Backbone curves
 
 N_info = NormalFormInfo.N;
 [damp, freq] = polarnormalform(N_info.coeff, N_info.exponents, N_info.phi);
 figure
-maxRho = abs(zData{indTest(1),2}(1,1));
-[dmp, frq, amp, rho_plot] = backbonecurves(damp, freq, SSMFunction, T, 1, maxRho,'norm');
+rhoCal = abs(zData{indTest(1),2}(1,1));
+[dmp, frq, amp, rho_plot] = backbonecurves(damp, freq, SSMFunction, T, 1, rhoCal,'norm');
 subplot(121); ylabel('$u \, [$m$]$','Interpreter','latex')
 subplot(122); ylabel('$u \, [$m$]$','Interpreter','latex')
 %% 
@@ -95,7 +96,7 @@ subplot(122); ylabel('$u \, [$m$]$','Interpreter','latex')
 
 % Compute with SSMTool
 f_full = 1e-3*[2 8];
-w_span = [30 37];
+w_span = abs(imag(lambda(1)))*[0.9 1.2];
 FRC_full = getFRC_full(M, C, K, fnl, f_vec, f_full, outdof, w_span, ROMOrder);
 
 %%
@@ -105,7 +106,19 @@ amp_max = max(FRC_full.(['F' num2str(idx_f_full)]).Amp);
 [~,pos] = min(abs(amp-amp_max));
 rho_max = rho_plot(pos);
 f_red = abs(damp(rho_max)*rho_max) * f_full/f_full(idx_f_full);
-ratio_force_data_full = f_red/f_full(idx_f_full);
+ratio_force_data_full = f_red/f_full(idx_f_full)
+
+calibrationLoads = [0.000001];
+calloadvector = calibrationLoads.*f_vec;
+ICCal = getStaticResponse(K, M, F, calloadvector, 0, 0);
+zCal = Tinv(V.'*repmat(ICCal(outdof,indTest),size(yData{1,2},1),1));
+rhoCal = abs(zCal(1));
+fpsi = fsolve(@(fpsi) [damp(rhoCal)*rhoCal + fpsi(1)*sin(fpsi(2)); ...
+    freq(rhoCal) - 0 + fpsi(1)/rhoCal*cos(fpsi(2))],...
+    [0; 0]);
+ratio = 0.5*abs(fpsi(1)/calibrationLoads(indTest));
+imag(lambda(1))*abs(NormalFormInfo.iT.lintransf*sum(V).'*(f_vec'*(K\f_vec)))
+% f_red = ratio*f_full;
 
 %%
 % Compute with data-driven model
@@ -115,7 +128,7 @@ FRC_data = getFRC_ddROM(ddROM,f_red,w_span,1);
 
 %%
 % Plot
-figure; hold on;
+figure(100); hold on;
 plot(frq, amp,'k','DisplayName', 'Backbone - SSMlearn')
 plot_FRC(FRC_full, 'r', 'SSMTool')
 plot_FRC(FRC_data, 'b', 'SSMLearn')
@@ -130,5 +143,5 @@ else
     load('data_sweep.mat','DataInfo','FRC_NI')
 end
 %%
-figure;
+figure(100);
 plot(FRC_NI.omega,FRC_NI.amp,'c.','MarkerSize',24,'DisplayName','Numerical integration')
